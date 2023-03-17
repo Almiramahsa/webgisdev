4e920a938275e73bcf1e1499c7d971a9a1172967
Almira Mahsa <almiramahsa9@gmail.com>
2023-03-17 15:17:23 +0700

    update with react leaflet

--- src/components/MapInit.jsx
+++ src/components/MapInit.jsx
@@ -1,19 +1,10 @@
-import React, { useEffect } from 'react';
-import L from 'leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
 function MapInit() {
-  useEffect(() => {
-    const map = L.map('mapid').setView([-2.5, 117], 4);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    }).addTo(map);
-  }, []);
-
   return (
-    <div>
-      <p className="font-bold text-4xl">TES</p>
-      <div id="mapid"></div>
-    </div>
+    <MapContainer center={[-2.5, 117]} zoom={4} id="mapid">
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="<attribution>" />
+    </MapContainer>
   );
 }
 
