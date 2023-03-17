59cf2f97c56dc988b1cfaba6724ab719c6f4b26e
Almira Mahsa <almiramahsa9@gmail.com>
2023-03-17 15:25:41 +0700

    edit basemap

--- src/components/MapInit.jsx
+++ src/components/MapInit.jsx
@@ -2,10 +2,14 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 
 function MapInit() {
   return (
-    <MapContainer center={[-2.5, 117]} zoom={4} id="mapid">
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="<attribution>" />
+    <MapContainer center={[-2.5, 117]} zoom={5} id="mapid">
+      <TileLayer
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+      />
     </MapContainer>
   );
 }
 
 export default MapInit;
+//<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="<attribution>" />
